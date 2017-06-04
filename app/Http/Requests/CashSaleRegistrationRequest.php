4275ed0c84329d6fccb2e2facbc07ea107409fdf
abin jose <abin111manjapra@gmail.com>
2017-06-05 02:24:56 +0530

    Sale registration validation rules fixed

--- app/Http/Requests/CashSaleRegistrationRequest.php
+++ app/Http/Requests/CashSaleRegistrationRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\Vehicle;
+use App\Models\Account;
+use App\Models\Product;
 
 class CashSaleRegistrationRequest extends FormRequest
 {
@@ -17,6 +21,42 @@ class CashSaleRegistrationRequest extends FormRequest
     }
 
     /**
+     * Customized error messages
+     *
+     */
+    public function messages()
+    {
+        return [
+            'vehicle_id_cash.required'              => "The truck number field is required.",
+            'vehicle_id_cash.integer'               => "Something went wrong. Please try again after reloading the page.",
+            'vehicle_id_cash.in'                    => "Something went wrong. Please try again after reloading the page.",
+            'purchaser_account_id_cash.required'    => "The purchaser field is required.",
+            'purchaser_account_id_cash.integer'     => "Something went wrong. Please try again after reloading the page.",
+            'purchaser_account_id_cash.in'          => "Something went wrong. Please try again after reloading the page.",
+            'product_id_cash.required'              => "The product field is required.",
+            'product_id_cash.integer'               => "Something went wrong. Please try again after reloading the page.",
+            'product_id_cash.in'                    => "Something went wrong. Please try again after reloading the page.",
+            'measure_type_cash.integer'             => "Something went wrong. Please try again after reloading the page.",
+            'measure_type_cash.in'                  => "Something went wrong. Please try again after reloading the page.",
+            'quantity_cash.required'                => "Required field.",
+            'quantity_cash.integer'                 => "Invalid data",
+            'quantity_cash.max'                     => "Maximum value exceeded",
+            'rate_cash.required'                    => "Required field.",
+            'rate_cash.integer'                     => "Invalid data",
+            'rate_cash.max'                         => "Maximum value exceeded",
+            'bill_amount_cash.required'             => "Required field.",
+            'bill_amount_cash.integer'              => "Invalid data",
+            'bill_amount_cash.max'                  => "Maximum value exceeded",
+            'discount_cash.required'                => "Required field.",
+            'discount_cash.integer'                 => "Invalid data",
+            'discount_cash.max'                     => "Maximum value exceeded",
+            'deducted_total_cash.required'          => "Required field.",
+            'deducted_total_cash.integer'           => "Invalid data",
+            'deducted_total_cash.max'               => "Maximum value exceeded",
+        ];
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -24,17 +64,78 @@ class CashSaleRegistrationRequest extends FormRequest
     public function rules()
     {
         return [
-            'vehicle_id_cash' => 'required',
-            'purchaser_account_id_cash' => 'required',
-            'date_cash' => 'required',
-            'time_cash' => 'required',
-            'product_id_cash' => 'required',
-            'measure_type_cash' => 'required',
-            'quantity_cash' => 'required',
-            'rate_cash' => 'required',
-            'bill_amount_cash' => 'required',
-            'discount_cash' => 'required',
-            'deducted_total_cash' => 'required',
+            'vehicle_id_cash'           => [
+                                                'required',
+                                                'integer',
+                                                Rule::in(Vehicle::pluck('id')->toArray()),
+                                            ],
+            'purchaser_account_id_cash' => [
+                                                'required',
+                                                'integer',
+                                                Rule::in(Account::pluck('id')->toArray()),
+                                            ],
+            'date_cash'                 => [
+                                                'required',
+                                                'date_format:d/m/Y',
+                                            ],
+            'time_cash'                 => [
+                                                'required',
+                                            ],
+            'product_id_cash'           => [
+                                                'required',
+                                                'integer',
+                                                Rule::in(Product::pluck('id')->toArray()),
+                                            ],
+            'measure_type_cash'         => [
+                                                'required',
+                                                'integer',
+                                                Rule::in(['1', '2']),
+                                            ],
+            'quantity_cash'             => [
+                                                'required',
+                                                'integer',
+                                                'max:2000'
+                                            ],
+            'rate_cash'                 => [
+                                                'required',
+                                                'numeric',
+                                                'max:9999'
+                                            ],
+            'bill_amount_cash'          => [    
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
+            'discount_cash'             => [
+                                                'required',
+                                                'numeric',
+                                                'max:9999'
+                                            ],
+            'deducted_total_cash'       => [
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
+            'old_balance'               => [
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
+            'total'                     => [
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
+            'paid_amount'               => [
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
+            'balance'                   => [
+                                                'required',
+                                                'numeric',
+                                                'max:99999'
+                                            ],
         ];
     }
 }
--- app/Http/Requests/CreditSaleRegistrationRequest.php
+++ app/Http/Requests/CreditSaleRegistrationRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\Vehicle;
+use App\Models\Account;
+use App\Models\Product;
 
 class CreditSaleRegistrationRequest extends FormRequest
 {
@@ -17,6 +21,42 @@ class CreditSaleRegistrationRequest extends FormRequest
     }
 
     /**
+     * Customized error messages
+     *
+     */
+    public function messages()
+    {
+        return [
+            'vehicle_id.required'           => "The truck number field is required.",
+            'vehicle_id.integer'            => "Something went wrong. Please try again after reloading the page.",
+            'vehicle_id.in'                 => "Something went wrong. Please try again after reloading the page.",
+            'purchaser_account_id.required' => "The purchaser field is required.",
+            'purchaser_account_id.integer'  => "Something went wrong. Please try again after reloading the page.",
+            'purchaser_account_id.in'       => "Something went wrong. Please try again after reloading the page.",
+            'product_id.required'           => "The product field is required.",
+            'product_id.integer'            => "Something went wrong. Please try again after reloading the page.",
+            'product_id.in'                 => "Something went wrong. Please try again after reloading the page.",
+            'measure_type.integer'          => "Something went wrong. Please try again after reloading the page.",
+            'measure_type.in'               => "Something went wrong. Please try again after reloading the page.",
+            'quantity.required'             => "Required field.",
+            'quantity.integer'              => "Invalid data",
+            'quantity.max'                  => "Maximum value exceeded",
+            'rate.required'                 => "Required field.",
+            'rate.integer'                  => "Invalid data",
+            'rate.max'                      => "Maximum value exceeded",
+            'bill_amount.required'          => "Required field.",
+            'bill_amount.integer'           => "Invalid data",
+            'bill_amount.max'               => "Maximum value exceeded",
+            'discount.required'             => "Required field.",
+            'discount.integer'              => "Invalid data",
+            'discount.max'                  => "Maximum value exceeded",
+            'deducted_total.required'       => "Required field.",
+            'deducted_total.integer'        => "Invalid data",
+            'deducted_total.max'            => "Maximum value exceeded",
+        ];
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -24,17 +64,58 @@ class CreditSaleRegistrationRequest extends FormRequest
     public function rules()
     {
         return [
-            'vehicle_id' => 'required',
-            'purchaser_account_id' => 'required',
-            'date' => 'required',
-            'time' => 'required',
-            'product_id' => 'required',
-            'measure_type' => 'required',
-            'quantity' => 'required',
-            'rate' => 'required',
-            'bill_amount' => 'required',
-            'discount' => 'required',
-            'deducted_total' => 'required',
+            'vehicle_id'            => [
+                                            'required',
+                                            'integer',
+                                            Rule::in(Vehicle::pluck('id')->toArray()),
+                                        ],
+            'purchaser_account_id'  => [
+                                            'required',
+                                            'integer',
+                                            Rule::in(Account::pluck('id')->toArray()),
+                                        ],
+            'date'                  => [
+                                            'required',
+                                            'date',
+                                        ],
+            'time'                  => [
+                                            'required',
+                                        ],
+            'product_id'            => [
+                                            'required',
+                                            'integer',
+                                            Rule::in(Product::pluck('id')->toArray()),
+                                        ],
+            'measure_type'          => [
+                                            'required',
+                                            'integer',
+                                            Rule::in(['1', '2']),
+                                        ],
+            'quantity'              => [
+                                            'required',
+                                            'integer',
+                                            'max:2000'
+                                        ],
+            'rate'                  => [
+                                            'required',
+                                            'numeric',
+                                            'max:9999'
+                                        ],
+            'bill_amount'           => [
+                                            'required',
+                                            'numeric',
+                                            'max:99999'
+                                        ],
+            'discount'              => [
+                                            'required',
+                                            'numeric',
+                                            'max:9999'
+                                        ],
+            'deducted_total'        => [
+                                            'required',
+                                            'numeric',
+                                            'max:99999'
+                                        ]
         ];
     }
 }
